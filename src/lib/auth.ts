14c634749f4585dce48aeb0f436534a56b44560e
Edwinfom00 <edwinfom05@gmail.com>
2026-03-21 12:10:39 +0100

    add source provider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -16,6 +16,12 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    },
+  },
   session: {
     expiresIn: 60 * 60 * 24 * 7,
     updateAge: 60 * 60 * 24,
